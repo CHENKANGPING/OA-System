9a0be0343451dfa1be4177468382fb84596333d9
HiiAen <164892170+CHENKANGPING@users.noreply.github.com>
2025-08-06 17:08:21 +0800

    2025/8/6添加了个人考勤页面的布局，发布考勤对话框的实现，发起考勤功能实现，个人考勤列表的展示

--- back/apps/absent/utlis.py
+++ back/apps/absent/utlis.py
@@ -1,13 +1,17 @@
 def get_responder(request):
     user = request.user
-    if user.department.leader.uid == user.uid:
+    
+    # 添加安全检查
+    if not user.department:
+        return None
+        
+    if user.department.leader and user.department.leader.uid == user.uid:
         # 1.1 如果是董事会
         if user.department.name == '董事会':
             responder = None
         else:
             responder = user.department.manager
-
-        # 2. 如果不是部门leader
+    # 2. 如果不是部门leader
     else:
         responder = user.department.leader
 
