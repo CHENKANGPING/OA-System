b408fca7d1cb353ec696c7e9809a7b67c503cc59
HiiAen <164892170+CHENKANGPING@users.noreply.github.com>
2025-08-06 17:06:49 +0800

    2025/8/6添加了个人考勤页面的布局，发布考勤对话框的实现，发起考勤功能实现，个人考勤列表的展示

---
+++ front/oafront/src/api/absentHttp.js
@@ -0,0 +1,31 @@
+import http from "@/api/http.js";
+
+const getAbsentTypes = () =>{
+    const path = "/absent/type"
+    return http.get(path)
+}
+
+
+const getResponder = () =>{
+    const path = "/absent/responder"
+    return http.get(path)
+}
+
+const applyAbsent = (data) =>{
+    const path = "/absent/absent"
+    return http.post(path,data)
+}
+
+const  getMyAbsents = (page=1) =>{
+    const path = "/absent/absent?who=my&page="+page
+    return http.get(path)
+
+}
+
+
+export default {
+    getAbsentTypes,
+    getResponder,
+    applyAbsent,
+    getMyAbsents
+}
