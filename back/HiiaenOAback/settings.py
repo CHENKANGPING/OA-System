d6bc0f17ac9ef934df6aa71d4e595dd630072f15
HiiAen <164892170+CHENKANGPING@users.noreply.github.com>
2025-08-07 17:40:39 +0800

    2025/8/7 完成了下属考勤页面的布局实现，各种组建的封装，bug解决，实现了处理考勤的功能

--- back/HiiaenOAback/settings.py
+++ back/HiiaenOAback/settings.py
@@ -149,7 +149,7 @@ REST_FRAMEWORK = {
         'apps.oaauth.authentications.JWTAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 1
+    'PAGE_SIZE': 10
 }
 
 APPEND_SLASH = False
