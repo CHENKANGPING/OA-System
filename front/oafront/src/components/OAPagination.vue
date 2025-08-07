a22cd80675b15f36a69d922acbc008f73cc72048
HiiAen <164892170+CHENKANGPING@users.noreply.github.com>
2025-08-07 17:40:36 +0800

    2025/8/7 完成了下属考勤页面的布局实现，各种组建的封装，bug解决，实现了处理考勤的功能

---
+++ front/oafront/src/components/OAPagination.vue
@@ -0,0 +1,28 @@
+<script setup name="OAPagination">
+// 移除了 defineModel 和 defineProps 的导入
+
+let props = defineProps({
+  total:{
+    type:Number,
+    default:0
+  },
+  pageSize:{
+    type:Number,
+    default:10
+  }
+})
+
+let page = defineModel({required:true})
+
+</script>
+
+<template>
+ <el-pagination v-model:current-page="page" :total="props.total"
+                       background layout="prev, pager, next"/>
+</template>
+
+<style scoped>
+.el-pagination {
+  justify-content: center;
+}
+</style>
