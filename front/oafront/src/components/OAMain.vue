823ac233f2c1c73225abdd18ac58bc8d03a65deb
HiiAen <164892170+CHENKANGPING@users.noreply.github.com>
2025-08-07 17:40:29 +0800

    2025/8/7 完成了下属考勤页面的布局实现，各种组建的封装，bug解决，实现了处理考勤的功能

---
+++ front/oafront/src/components/OAMain.vue
@@ -0,0 +1,22 @@
+<script setup>
+import OAPageHeader from "@/components/OAPageHeader.vue";
+// 移除了 defineProps 的导入
+
+let props = defineProps({
+  title: ""
+})
+
+</script>
+
+<template>
+  <el-space direction="vertical" fill style="width: 100%">
+    <OAPageHeader :content="props.title"></OAPageHeader>
+    <slot></slot>
+  </el-space>
+</template>
+
+<style scoped>
+.el-space :deep(.el-space__item) {
+  width: 100%;
+}
+</style>
