4aa42ff19c7acc0b6d05fdb42bf1eb7303ecb279
HiiAen <164892170+CHENKANGPING@users.noreply.github.com>
2025-08-08 16:02:43 +0800

    2025/8/8 完成了通知相关模型的创建以及序列化，发布通知后端的功能实现，搭建了通知页面以及富文本编辑器的使用

--- back/HiiaenOAback/urls.py
+++ back/HiiaenOAback/urls.py
@@ -20,4 +20,6 @@ urlpatterns = [
 
     path('auth/', include('apps.oaauth.urls')),
     path('absent/', include('apps.absent.urls')),
+    path('inform/',include('apps.inform.urls')),
+    path('staff/',include('apps.staff.urls')),
 ]
