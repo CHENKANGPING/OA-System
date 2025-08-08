ef23c104a2b8385a1f081fc7cc165f6cf4ff3e66
HiiAen <164892170+CHENKANGPING@users.noreply.github.com>
2025-08-08 16:02:02 +0800

    2025/8/8 完成了通知相关模型的创建以及序列化，发布通知后端的功能实现，搭建了通知页面以及富文本编辑器的使用

--- front/oafront/src/router/index.js
+++ front/oafront/src/router/index.js
@@ -4,6 +4,9 @@ import frame from '../views/main/frame.vue'
 import {useAuthStore} from '@/stores/auth';
 import myabsent from '@/views/absent/my.vue'
 import subabsent from '@/views/absent/sub.vue'
+import publish from '@/views/inform/publish.vue'
+import inform_list from '@/views/inform/list.vue'
+import detail from '@/views/inform/detail.vue'
 
 const router = createRouter({
     history: createWebHashHistory(import.meta.env.BASE_URL),
@@ -14,7 +17,11 @@ const router = createRouter({
             component: frame,
             children: [
                 {path: '/absent/my', name: 'myabsent', component: myabsent},
-                {path: '/absent/sub', name: 'subabsent', component: subabsent}
+                {path: '/absent/sub', name: 'subabsent', component: subabsent},
+                {path:'/inform/publish', name:'inform_publish',component: publish},
+                {path:'/inform/list', name:'inform_list',component: inform_list},
+                {path:'/inform/detail', name:'inform_detail',component: detail},
+
             ]
         },
         {
