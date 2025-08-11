9c2e341c2b70d7a0e0f2c4d87ba9f140cdf0699c
HiiAen <164892170+CHENKANGPING@users.noreply.github.com>
2025-08-11 17:10:10 +0800

    2025/8/11 完成了富文本编辑器上传图片的功能，搭建了通知列表的页面，能够按需显示通知和删除通知

--- back/apps/inform/urls.py
+++ back/apps/inform/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'inform'
 
 router = DefaultRouter(trailing_slash=False)
+# 注册为 'inform' 路径
 router.register('inform',views.InformViewSet,basename='inform')
 
 urlpatterns = [] +router.urls
