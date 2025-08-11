4ac893b51bb25f53d0a4cb6d2446275969424c5c
HiiAen <164892170+CHENKANGPING@users.noreply.github.com>
2025-08-11 17:10:01 +0800

    2025/8/11 完成了富文本编辑器上传图片的功能，搭建了通知列表的页面，能够按需显示通知和删除通知

--- front/oafront/src/views/inform/list.vue
+++ front/oafront/src/views/inform/list.vue
@@ -1,11 +1,106 @@
-<script setup name="informlist">
+<script name="informlist" setup>
+import OAMain from "@/components/OAMain.vue";
+import OAPagination from "@/components/OAPagination.vue";
+import timeFormatter from "@/utlis/timeFormatter.js";
+import { onMounted, reactive, ref } from "vue";
+import { useAuthStore } from "@/stores/auth.js";
+import informHttp from "@/api/informHttp.js";
+import { ElMessage } from "element-plus";
+import OADialog from "@/components/OADialog.vue";
+
+
+
+
+const authStore = useAuthStore()
+let informs = ref([])
+let pageination = reactive({
+  page: 1,
+  tota: 0
+})
+
+let dialogVisible = ref(false)
+let handleIndex = 0
+
+const onShowDialog = (index) => {
+  handleIndex = index
+  dialogVisible.value = true
+}
+
+const onDeleteInform = async () => {
+  try {
+    let inform = informs.value[handleIndex]
+    await informHttp.deleteInform(inform.id)
+    informs.value.splice(handleIndex, 1)
+    ElMessage.success("删除成功")
+    dialogVisible.value = false
+  } catch (detail) {
+    ElMessage.error(detail)
+  }
+
+}
+
+onMounted(async () => {
+  try {
+    let data = await informHttp.getInformList(1)
+    pageination.total = data.count
+    informs.value = data.results
+  } catch (detail) {
+    ElMessage.error(detail)
+  }
+})
 
 </script>
 
 <template>
+  <OADialog v-model="dialogVisible" title="提示" @submit="onDeleteInform">
 
+    <span>您确定删除吗！</span>
+  </OADialog>
+  <OAMain title="通知列表">
+    <el-card>
+      <el-table :data="informs" style="width: 100%">
+        <el-table-column label="标题">
+          <template #default="scope">
+            <router-link :to="{ name: 'inform_detail', params: { pk: scope.row.id } }">
+              {{ scope.row.title }}
+            </router-link>
+          </template>
+        </el-table-column>
+        <el-table-column label="发布者">
+          <template #default="scope">
+            {{ '[' + scope.row.author.department.name + ']' + scope.row.author.realname }}
+          </template>
+        </el-table-column>
+        <el-table-column label="发布时间">
+          <template #default="scope">
+            {{ timeFormatter.stringFromDateTime(scope.row.create_time) }}
+          </template>
+        </el-table-column>
+        <el-table-column label="部门可见">
+          <template #default="scope">
+            <el-tag v-if="scope.row.public" type="success">公开</el-tag>
+            <el-tag v-for="department in scope.row.departments" v-else :key="department.name" type="info">
+              {{ department.name }}
+            </el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作">
+          <template #default="scope">
+            <el-button v-if="scope.row.author.uid == authStore.user.uid" icon="Delete" type="danger"
+              @click="onShowDialog(scope.$index)" />
+            <el-button v-else disabled type="default">无</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <template #footer>
+        <OAPagination v-model="pageination.page" :total="pageination.total"></OAPagination>
+      </template>
+    </el-card>
+  </OAMain>
 </template>
 
 <style scoped>
-
+.el-tag {
+  margin-right: 4px;
+}
 </style>
