c43317a7cd543e9e02e6a9d7af1d38a65ce4c2d1
HiiAen <164892170+CHENKANGPING@users.noreply.github.com>
2025-08-12 16:32:00 +0800

    2025/8/12 完成了通知详情页的展示和数据处理，通知列表已读未读的状态显示，添加员工页面搭建

--- back/apps/inform/urls.py
+++ back/apps/inform/urls.py
@@ -1,11 +1,14 @@
 from rest_framework.routers import DefaultRouter
+from rest_framework.urls import path
+
 from . import views
 
 app_name = 'inform'
 
 router = DefaultRouter(trailing_slash=False)
 # 注册为 'inform' 路径
-router.register('inform',views.InformViewSet,basename='inform')
+router.register('inform', views.InformViewSet, basename='inform')
 
-urlpatterns = [] +router.urls
-
+urlpatterns = [
+                  path('inform/read', views.ReadInformView.as_view(), name="inform_read")
+              ] + router.urls
