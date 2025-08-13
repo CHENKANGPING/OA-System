840ba58e8fcfbb30d95e78fbf59c1604dd9c7b74
HiiAen <164892170+CHENKANGPING@users.noreply.github.com>
2025-08-13 16:54:43 +0800

    2025/8/13 完成了添加员工的后端实现，能够发送激活员工邮件，配合celery实现异步发送邮件，添加员工前端功能实现以及激活员工模板

--- back/apps/staff/urls.py
+++ back/apps/staff/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-
-from apps.staff.views import DepartmentListView
+from . import views
 
 app_name = 'staff'
 
 urlpatterns = [
-    path('departments', DepartmentListView.as_view(), name='departments'),
+    path('departments', views.DepartmentListView.as_view(), name='departments'),
+    path('staff', views.StaffView.as_view(), name='staff_views'),
+    path('active', views.ActiveStaffView.as_view(), name='active'),
+    path('test/celery', views.TestCeleryView.as_view(), name='test_celery'),
+
 ]
