219ce8df0e60ea2f407033f1693fb43554741f6f
HiiAen <164892170+CHENKANGPING@users.noreply.github.com>
2025-08-14 16:46:00 +0800

    2025/8/14 完成了激活员工功能的实现以及后端员工列表的实现，能够分页，员工状态修改的后端实现

--- back/apps/staff/serializers.py
+++ back/apps/staff/serializers.py
@@ -18,3 +18,21 @@ class AddStaffSerializer(serializers.Serializer):
         if request.user.department.leader.uid != request.user.uid:
             raise serializers.ValidationError('非部门leader不能添加员工')
         return attrs
+
+
+class ActiveStaffSerializer(serializers.Serializer):
+    email = serializers.EmailField(error_messages={'required': '请输入邮箱', 'invalid': '请输入正确格式的邮箱'})
+    password = serializers.CharField(max_length=20, error_messages={'required': '请输入密码'})
+
+    def validate(self,attrs):
+        email = attrs['email']
+        password = attrs['password']
+        try:
+            user = OAUser.objects.get(email=email)
+        except OAUser.DoesNotExist:
+            raise serializers.ValidationError('邮箱或密码错误！')
+        
+        if not user.check_password(password):
+            raise serializers.ValidationError('邮箱或密码错误！')
+        attrs['user'] = user
+        return attrs
