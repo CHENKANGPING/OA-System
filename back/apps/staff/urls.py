c915de7868dc2a7e0c4912be1c33e321529ab5b6
HiiAen <164892170+CHENKANGPING@users.noreply.github.com>
2025-08-14 16:45:53 +0800

    2025/8/14 完成了激活员工功能的实现以及后端员工列表的实现，能够分页，员工状态修改的后端实现

--- back/apps/staff/urls.py
+++ back/apps/staff/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
 from . import views
+from rest_framework.routers import DefaultRouter
+
+
+
 
 app_name = 'staff'
+router = DefaultRouter(trailing_slash=False)
+router.register(r'staff', views.StaffViewSet, basename='staff')
 
 urlpatterns = [
     path('departments', views.DepartmentListView.as_view(), name='departments'),
-    path('staff', views.StaffView.as_view(), name='staff_views'),
+    # path('staff', views.StaffView.as_view(), name='staff_views'),
     path('active', views.ActiveStaffView.as_view(), name='active'),
     path('test/celery', views.TestCeleryView.as_view(), name='test_celery'),
 
-]
+] + router.urls
