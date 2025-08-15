d5e29d21a4037dfb615ef65fb2df2e72cce6c0f0
HiiAen <164892170+CHENKANGPING@users.noreply.github.com>
2025-08-15 16:51:48 +0800

    2025/8/15 完成了员工修改状态的功能，在员工列表中添加了搜索框，能够完成检索功能

--- front/oafront/src/api/staffHttp.js
+++ front/oafront/src/api/staffHttp.js
@@ -14,13 +14,25 @@ const addStaff = (realname,email,password) =>{
     })
 }
 
-const getStaffList = (page=1,size=10) =>{
-    const path = `/staff/staff?page=${page}&size=${size}`
-    return http.get(path)
+const getStaffList = (page=1,size=10,params) =>{
+    const path = `/staff/staff`
+    params = params?params:{}
+    params['page'] = page
+    params['size'] = size
+
+    return http.get(path,params)
+}
+
+const updateStaffStatus =(staff_id,status) =>{
+    const path  = "/staff/staff/" + staff_id
+    return http.put(path,{
+        status
+    })
 }
 
 export default {    
     getALLDepartment,
     addStaff,
-    getStaffList
+    getStaffList,
+    updateStaffStatus
 }
