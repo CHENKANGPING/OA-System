f42e1f613614634bc2c5a95d85b2be977e0756b9
HiiAen <164892170+CHENKANGPING@users.noreply.github.com>
2025-08-18 17:34:47 +0800

    2025/8/18 完成了员工信息的批量导出和上传，可以直接上传自动批量发送邮件。

--- back/apps/staff/urls.py
+++ back/apps/staff/urls.py
@@ -13,6 +13,10 @@ urlpatterns = [
     path('departments', views.DepartmentListView.as_view(), name='departments'),
     # path('staff', views.StaffView.as_view(), name='staff_views'),
     path('active', views.ActiveStaffView.as_view(), name='active'),
+    path('download', views.SraffDownloadView.as_view(), name='download'),
+    path('upload', views.StaffUploadView.as_view(), name='upload'),
+
+
     path('test/celery', views.TestCeleryView.as_view(), name='test_celery'),
 
 ] + router.urls
