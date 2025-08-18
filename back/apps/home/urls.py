c9a605e7a6afb010076bf6ce508c188c6602f87d
HiiAen <164892170+CHENKANGPING@users.noreply.github.com>
2025-08-18 17:34:44 +0800

    2025/8/18 完成了员工信息的批量导出和上传，可以直接上传自动批量发送邮件。

---
+++ back/apps/home/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+    
+    
+
+appp_name = 'home'
+
+urlpatterns = [
+    path('latest/inform', views.LatestInformView.as_view(),name='latest_inform'),
+    path('latest/absent', views.LatestAbsentView.as_view(),name='latest_absent'),
+    path('department/staff/count', views.DepartmentStaffCountView.as_view(),name='department_staff_count'),
+
+]
