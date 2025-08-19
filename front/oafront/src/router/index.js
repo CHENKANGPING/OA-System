b4c36095311bcc50ab503a1d8cc8dac3d03cebb2
HiiAen <164892170+CHENKANGPING@users.noreply.github.com>
2025-08-19 17:22:06 +0800

    2025/8/19 完成了首页的展示，包括员工柱形图，通知看板，请假看板，使用redis缓存了首页的数据，重构了侧边栏路由

--- front/oafront/src/router/index.js
+++ front/oafront/src/router/index.js
@@ -1,40 +1,11 @@
 import {createRouter, createWebHashHistory} from 'vue-router'
-import login from '../views/login/login.vue'
-import frame from '../views/main/frame.vue'
 import {useAuthStore} from '@/stores/auth';
-import myabsent from '@/views/absent/my.vue'
-import subabsent from '@/views/absent/sub.vue'
-import publish from '@/views/inform/publish.vue'
-import inform_list from '@/views/inform/list.vue'
-import detail from '@/views/inform/detail.vue'
-import staffadd from '@/views/staff/add.vue'
-import stafflist from "@/views/staff/list.vue"
+import frame_routes from "@/router/frame.js"
+import login_routes from "@/router/login.js"
 
 const router = createRouter({
     history: createWebHashHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-            path: '/',
-            name: 'frame',
-            component: frame,
-            children: [
-                {path: '/absent/my', name: 'myabsent', component: myabsent},
-                {path: '/absent/sub', name: 'subabsent', component: subabsent},
-                {path: '/inform/publish', name: 'inform_publish', component: publish},
-                {path: '/inform/list', name: 'inform_list', component: inform_list},
-                {path: '/inform/detail/:pk', name: 'inform_detail', component: detail},
-                {path: '/staff/add', name: 'staff_add', component: staffadd},
-                {path: '/staff/list', name: 'staff_list', component: stafflist},
-
-
-            ]
-        },
-        {
-            path: '/login',
-            name: 'login',
-            component: login
-        }
-    ]
+    routes: frame_routes.concat(login_routes)
 })
 
 router.beforeEach((to, from) => {
