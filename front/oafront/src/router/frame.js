978f6edccb260ecd3197708d457153afa4f64e15
HiiAen <164892170+CHENKANGPING@users.noreply.github.com>
2025-08-20 17:55:08 +0800

    2025/8/19 完成了权限管理，修了一些bug

--- front/oafront/src/router/frame.js
+++ front/oafront/src/router/frame.js
@@ -7,98 +7,157 @@ import detail from "@/views/inform/detail.vue";
 import staffadd from "@/views/staff/add.vue";
 import stafflist from "@/views/staff/list.vue";
 import home from "@/views/home/home.vue";
-import absent from "@/views/absent/index.vue"
-import inform from "@/views/inform/index.vue"
-import staff from "@/views/staff/index.vue"
+import absent from "@/views/absent/index.vue";
+import inform from "@/views/inform/index.vue";
+import staff from "@/views/staff/index.vue";
+import { PermissionChoices } from "@/stores/auth";
 
 const routes = [
-    {
+  {
+    path: "/",
+    name: "frame",
+    component: frame,
+    children: [
+      {
         path: "/",
-        name: "frame",
-        component: frame,
+        name: "home",
+        component: home,
+        meta: {
+          icon: "HomeFilled",
+          text: "首页",
+          permissions: [PermissionChoices.Staff], 
+          opt: "|",
+        },
+      },
+      {
+        path: "/absent",
+        name: "absent",
+        component: absent,
+        meta: {
+          icon: "Checked",
+          text: "考勤管理",
+          permissions: [PermissionChoices.Staff], 
+          opt: "|",
+        },
         children: [
-            {
-                path: "/", name: "home", component: home,
-                meta: {
-                    icon: "HomeFilled",
-                    text: "首页"
-                },
+          {
+            path: "my",
+            name: "myabsent",
+            component: myabsent,
+            meta: {
+              icon: "UserFilled",
+              text: "个人考勤",
+              permissions: [PermissionChoices.Staff], 
+              opt: "|",
             },
-            {
-                path: "/absent", name: "absent", component: absent,
-                meta: {
-                    icon: "Checked",
-                    text: "考勤管理"
-                },
-                children: [
-                    {
-                        path: "/my", name: "myabsent", component: myabsent,
-                        meta: {
-                            icon: "UserFilled",
-                            text: "个人考勤"
-                        }
-                    },
-                    {
-                        path: "/sub", name: "subabsent", component: subabsent,
-                        meta: {
-                            icon: "User",
-                            text: "下属考勤"
-                        },
-                    },
-
-                ]
+          },
+          {
+            path: "sub",
+            name: "subabsent",
+            component: subabsent,
+            meta: {
+              icon: "User",
+              text: "下属考勤",
+              permissions: [
+                PermissionChoices.Boarder,
+                PermissionChoices.Leader, 
+              ],
+              opt: "|",
             },
-            {
-                path: "/inform", name: "inform", component: inform,
-                meta: {
-                    icon: "BellFilled",
-                    text: "通知管理"
-                },
-                children: [
-                    {
-                        path: "/publish", name: "inform_publish", component: publish,
-                        meta: {
-                            icon: "BellFilled",
-                            text: "发布通知"
-                        },
-                    },
-                    {
-                        path: "/list", name: "inform_list", component: inform_list,
-                        meta: {
-                            icon: "Tickets",
-                            text: "通知列表"
-                        },
-                    },
-                    {path: "/detail/:pk", name: "inform_detail", component: detail},
-                ]
-            },
-            {
-                path: "/staff", name: staff, component: staff,
-                meta: {
-                    icon: "Avatar",
-                    text: "员工管理"
-                },
-                children: [
-                    {
-                        path: "/add", name: "staff_add", component: staffadd,
-                        meta: {
-                            icon: "Plus",
-                            text: "新增员工"
-                        },
-                    },
-                    {
-                        path: "/list", name: "staff_list", component: stafflist,
-                        meta: {
-                            icon: "Tickets",
-                            text: "员工列表"
-                        },
-                    },
-
-                ]
-            },
-
+          },
         ],
-    },
-]
+      },
+      {
+        path: "/inform",
+        name: "inform",
+        component: inform,
+        meta: {
+          icon: "BellFilled",
+          text: "通知管理",
+          permissions: [PermissionChoices.Staff], 
+          opt: "|",
+        },
+        children: [
+          {
+            path: "publish",
+            name: "inform_publish",
+            component: publish,
+            meta: {
+              icon: "BellFilled",
+              text: "发布通知",
+              permissions: [
+                PermissionChoices.Boarder,
+                PermissionChoices.Leader, 
+              ],
+              opt: "|",
+            },
+          },
+          {
+            path: "list",
+            name: "inform_list",
+            component: inform_list,
+            meta: {
+              icon: "Tickets",
+              text: "通知列表",
+              permissions: [PermissionChoices.Staff], 
+              opt: "|",
+            },
+          },
+          {
+            path: "detail/:pk",
+            name: "inform_detail",
+            component: detail,
+            meta: {
+              hidden: true,
+              permissions: [PermissionChoices.Staff], 
+              opt: "|",
+            },
+          },
+        ],
+      },
+      {
+        path: "/staff",
+        name: "staff",
+        component: staff,
+        meta: {
+          icon: "Avatar",
+          text: "员工管理",
+          permissions: [PermissionChoices.Boarder, PermissionChoices.Leader], 
+          opt: "|",
+        },
+        children: [
+          {
+            path: "add",
+            name: "staff_add",
+            component: staffadd,
+            meta: {
+              icon: "Plus",
+              text: "新增员工",
+              permissions: [
+                PermissionChoices.Boarder,
+                PermissionChoices.Leader, 
+              ],
+              opt: "|",
+            },
+          },
+          {
+            path: "list",
+            name: "staff_list",
+            component: stafflist,
+            meta: {
+              icon: "Tickets",
+              text: "员工列表",
+              permissions: [
+                PermissionChoices.Boarder,
+                PermissionChoices.Leader, 
+              ],
+              opt: "|",
+            },
+          },
+        ],
+      },
+    ],
+  },
+];
 
 export default routes;
